07b9ebd0b5b053048a3c273ca2bf95ab690fafb6
Matt Dennewitz <mattdennewitz@gmail.com>
2014-05-26 22:44:29 -0500

    localize time to ET before shipping out

--- homers/serializers.py
+++ homers/serializers.py
@@ -1,7 +1,14 @@
+import pytz
+
 from flask import url_for
 
 
+et_tz = pytz.timezone('America/New_York')
+
 def serialize_play(play):
+    play_time = pytz.utc.localize(play.at)
+    play_time = play_time.astimezone(et_tz)
+
     return {
         'batter': {
             'name': play.batter.get_full_name(),
@@ -14,5 +21,5 @@ def serialize_play(play):
                 'team': play.pitcher_team,
             },
             'url': url_for('view_play', content_id=play.content_id),
-        'at': play.at.isoformat(),
+        'at': play_time.strftime('%Y-%m-%dT%H:%M:%S%z'),
     }
