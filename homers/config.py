b94f765cac88750e31103620db4cc2dbbaa556d2
Matt Dennewitz <mattdennewitz@gmail.com>
2015-09-06 14:02:15 -0400

    removed static and css

--- homers/config.py
+++ homers/config.py
@@ -3,16 +3,14 @@ import os
 
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://localhost/homers')
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'DATABASE_URL', 'postgres://localhost/homers')
+
 DATA_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__),
                  os.pardir,
                  'game-data'))
-
-# app settings
-PER_PAGE = 20
-DAYS_PER_PAGE = 7
-
 
 # allow for config overrides
 try:
